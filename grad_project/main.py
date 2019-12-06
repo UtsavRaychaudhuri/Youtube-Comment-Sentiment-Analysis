d76007736f45ce921789420d2d4a55013e865aa0
nishna <aleti.pdx.edu>
2019-12-06 12:11:25 -0800

    downling gifs into local machine

--- grad_project/main.py
+++ grad_project/main.py
@@ -17,8 +17,8 @@ from datetime import datetime
 import logging
 import os
 
-from flask import Flask, redirect, render_template, request, jsonify
-
+from flask import Flask, redirect, render_template, request, jsonify, Response, send_file
+import mimetypes
 from google.cloud import datastore
 from google.cloud import storage
 from google.cloud import vision
@@ -30,6 +30,7 @@ from pprint import pprint
 from PIL import Image
 import requests
 from io import BytesIO
+import urllib.request
 
 CLOUD_STORAGE_BUCKET = os.environ.get('CLOUD_STORAGE_BUCKET')
 
@@ -59,9 +60,75 @@ def submit():
   print(api_response.data[i].images.original.url)'''
  except ApiException as e:
   print("Exception when calling DefaultApi->gifs_search_get: %s\n" % e)
- response = requests.get(url)
- img = Image.open(BytesIO(response.content))
- return 'all ok' 
+ r = requests.get(url)
+ img = Image.open(BytesIO(r.content))
+ return Response(r,mimetype="image/gif",headers={"Content-disposition": "attachment; filename=myplot.gif"})
+""" response = requests.get(url)
+ img = Image.open(urllib.request.urlopen(url))
+ # Create a Cloud Storage client.
+ storage_client = storage.Client()
+
+    # Get the bucket that the file will be uploaded to.
+ bucket = storage_client.get_bucket(CLOUD_STORAGE_BUCKET)
+
+    # Create a new blob and upload the file's content.
+ blob = bucket.blob(img.filename)
+ return (jsonify(result=blob))
+ blob.upload_from_string(photo.read(),content_type=Image.open(urllib.request.urlopen(url)))
+
+    # Make the blob publicly viewable.
+ blob.make_public()
+
+    # Create a Cloud Vision client.
+ vision_client = vision.ImageAnnotatorClient()
+
+    # Use the Cloud Vision client to detect a face for our image.
+# Use the Cloud Vision client to detect a face for our image.
+ source_uri = 'gs://{}/{}'.format(CLOUD_STORAGE_BUCKET, blob.name)
+ image = vision.types.Image( source=vision.types.ImageSource(gcs_image_uri=source_uri))
+ faces = vision_client.face_detection(image).face_annotations
+
+    # If a face is detected, save to Datastore the likelihood that the face
+    # displays 'joy,' as determined by Google's Machine Learning algorithm.
+ if len(faces) > 0:
+  face = faces[0]
+
+        # Convert the likelihood string.
+  likelihoods = ['Unknown', 'Very Unlikely', 'Unlikely', 'Possible', 'Likely','Very Likely']
+  face_joy = likelihoods[face.joy_likelihood]
+ else:
+  face_joy = 'Unknown'
+
+    # Create a Cloud Datastore client.
+ datastore_client = datastore.Client()
+
+    # Fetch the current date / time.
+ current_datetime = datetime.now()
+
+    # The kind for the new entity.
+ kind = 'Faces'
+
+    # The name/ID for the new entity.
+ name = blob.name
+
+    # Create the Cloud Datastore key for the new entity.
+ key = datastore_client.key(kind, name)
+
+    # Construct the new entity using the key. Set dictionary values for entity
+    # keys blob_name, storage_public_url, timestamp, and joy.
+ entity = datastore.Entity(key)
+ entity = datastore.Entity(key)
+ entity['blob_name'] = blob.name
+ entity['image_public_url'] = blob.public_url
+ entity['timestamp'] = current_datetime
+ entity['joy'] = face_joy
+
+    # Save the new entity to Datastore.
+ datastore_client.put(entity)
+
+    # Redirect to the home page.
+ return redirect('/')"""
+
 @app.route('/')
 def homepage():
     # Create a Cloud Datastore client.
@@ -75,7 +142,7 @@ def homepage():
     # Return a Jinja2 HTML template and pass in image_entities as a parameter.
     return render_template('homepage.html', image_entities=image_entities)
 
-
+ 
 @app.route('/upload_photo', methods=['GET', 'POST'])
 def upload_photo():
     photo = request.files['file']
@@ -145,12 +212,10 @@ def upload_photo():
     # Redirect to the home page.
     return redirect('/')
 
-
 @app.errorhandler(500)
 def server_error(e):
     logging.exception('An error occurred during a request.')
-    return """
-    An internal error occurred: <pre>{}</pre>
+    return """ An internal error occurred: <pre>{}</pre>
     See logs for full stacktrace.
     """.format(e), 500
 
