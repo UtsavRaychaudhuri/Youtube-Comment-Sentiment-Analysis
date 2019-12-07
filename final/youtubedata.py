a516ed7c5a4246acaf6248e53b00a3c49022fd5c
Utsav Raychaudhuri <utsav@pdx.edu>
2019-12-07 05:24:49 +0000

    Adding initial code for pulling youtube data

---
+++ final/youtubedata.py
@@ -0,0 +1,15 @@
+import csv
+import os
+import pickle
+
+import google.oauth2.credentials
+
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# The CLIENT_SECRETS_FILE variable specifies the name of a file that contains
+# the OAuth 2.0 information for this application, including its client_id and
+# client_secret.
+CLIENT_SECRETS_FILE = "client_secret.json"
