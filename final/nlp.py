93c064d310e3a1f15ee67309c80860bd059bbe7b
Utsav Raychaudhuri <utsav@pdx.edu>
2019-12-07 21:14:42 +0000

    Making changes to get_sentiment_from reviews

--- final/nlp.py
+++ final/nlp.py
@@ -8,14 +8,15 @@ from youtubedata import callme_for_fetching_comments
 # Instantiates a client
 client = language.LanguageServiceClient()
 
-def get_sentiment_from_reviews():
+def get_sentiment_from_reviews(search_text):
     #Calling videointelligence for fetching comments
-    callme_for_fetching_comments()
+    callme_for_fetching_comments(search_text)
     #opens the comments.csv file for reading
     with open('comments.csv', mode='r') as csv_file:
     #converts csv into python dict for easy reading
     csv_reader = csv.DictReader(csv_file)
     myvideodb={}
+    comment_analysis = {}
     #inserts a {"key":"value" of the form {"Video Title":"Comments on the video" into myvideodb}
     for row in csv_reader:
         if row['Title'] in myvideodb:
@@ -31,3 +32,5 @@ def get_sentiment_from_reviews():
         # Detects the sentiment of the text
         sentiment = client.analyze_sentiment(document=document).document_sentiment
         print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
+        comment_analysis[title]={"Score":sentiment.score,"Magnitude":sentiment.magnitude}
+    return comment_analysis
