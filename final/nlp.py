55ef8f40ee9f1c0dd53443d72d03226f78bbb4ed
Utsav Raychaudhuri <utsav@pdx.edu>
2019-12-08 00:28:37 +0000

    Removing print statements

--- final/nlp.py
+++ final/nlp.py
@@ -15,22 +15,22 @@ def get_sentiment_from_reviews(search_text):
     with open('comments.csv', mode='r') as csv_file:
     #converts csv into python dict for easy reading
         csv_reader = csv.DictReader(csv_file)
-    myvideodb={}
-    comment_analysis = {}
-    #inserts a {"key":"value" of the form {"Video Title":"Comments on the video" into myvideodb}
-    for row in csv_reader:
-        if row['Title'] in myvideodb:
-            myvideodb[row['Title']]+=row['Comment'] + ' '
-        else:
-            myvideodb[row['Title']]=row['Comment'] + ' '
-    for title in myvideodb:
-        print(title)
-        document = types.Document(
-        content=myvideodb[title],
-        type=enums.Document.Type.PLAIN_TEXT)
+        myvideodb={}
+        comment_analysis = {}
+        #inserts a {"key":"value" of the form {"Video Title":"Comments on the video" into myvideodb}
+        for row in csv_reader:
+            if row['Title'] in myvideodb:
+                myvideodb[row['Title']]+=row['Comment'] + ' '
+            else:
+                myvideodb[row['Title']]=row['Comment'] + ' '
+        for title in myvideodb:
+            print(title)
+            document = types.Document(
+            content=myvideodb[title],
+            type=enums.Document.Type.PLAIN_TEXT)
 
-        # Detects the sentiment of the text
-        sentiment = client.analyze_sentiment(document=document).document_sentiment
-        print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
-        comment_analysis[title]={"Score":sentiment.score,"Magnitude":sentiment.magnitude}
-    return comment_analysis
+            # Detects the sentiment of the text
+            sentiment = client.analyze_sentiment(document=document).document_sentiment
+            # print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
+            comment_analysis[title]={"Score":sentiment.score,"Magnitude":sentiment.magnitude}
+        return comment_analysis
