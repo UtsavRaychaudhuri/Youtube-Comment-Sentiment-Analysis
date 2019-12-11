83427ae92ae8a04becf93cd2f8a6ac1043c573c2
nishna <aleti.pdx.edu>
2019-12-11 00:51:41 -0800

    adding doc strings

--- final/app.py
+++ final/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, request, jsonify, Response
+from flask import Flask, redirect, render_template, request, Response
 from nlp import get_sentiment_from_reviews
 from get_gif import get_gif_from_api
 from gif_face_recognition import face_recognition
@@ -7,24 +7,33 @@ app = Flask(__name__)
 
 @app.route('/',methods=['GET'])
 def home():
+
     # renders to the homepage, youtube-giphy.html
     return render_template('youtube-giphy.html')
 
 
 @app.route('/giphy',methods=['GET'])
 def giphy():
+
     #renders to gif.html page
     return render_template('gif.html')
 
 
 @app.route('/download', methods=['GET','POST'])
 def submit():
+    '''
+    Handles word from the form and passes the word to get_gif_from_api
+    and downloads the GIF into local machine
+    '''
+
     #Handles word from the form and passes the word to get_gif_from_api present in get_gif.py
     query = request.form['word']
     url_content=get_gif_from_api(query)
+
     #If there are no GIF's based on the given word, it will redirect to gif.html
     if url_content=='None':
        return render_template('gif.html')
+
     #Downloads GIF into the local machine
     else:
        return Response(url_content,mimetype="image/gif",headers={"Content-disposition": "attachment; filename="+query+'.gif'})
@@ -32,14 +41,24 @@ def submit():
 
 @app.route('/upload_photo', methods=['GET', 'POST'])
 def upload_photo():
+     '''
+     passes the photo to face_recognition
+     renders gif_view with joy, anger and suprise likelihood
+     '''
+
      #calling file which was uploaded, from gif.html
      photo = request.files['file']
+
      if not photo:
+
          #If submitted without uploading any photo it will redirect to gif.html.
          return render_template('gif.html')
+
      else:
+
          #passes photo to face_recognition function present in gif_face_recognition.py
          facial_expression=face_recognition(photo)
+
          # Redirects to the gif_view.html page to print the facial expressions
          return render_template('gif_view.html', facial_expression=facial_expression)
 
