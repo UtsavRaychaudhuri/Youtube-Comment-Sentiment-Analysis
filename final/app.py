2b90099fa22b772adf94eaf4a3f9f1a7363c1901
Utsav Raychaudhuri <utsav@pdx.edu>
2019-12-11 00:26:02 +0000

    Adding some changes to logger

--- final/app.py
+++ final/app.py
@@ -66,7 +66,7 @@ def analyse_comments():
 
 @app.errorhandler(500)
 def server_error(e):
-    logging.exception('An error occurred during a request.')
+    app.logger.info('An error occurred during a request.')
     return """ An internal error occurred: <pre>{}</pre>
     See logs for full stacktrace.
     """.format(e), 500
